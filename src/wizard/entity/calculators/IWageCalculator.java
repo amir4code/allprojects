c1f089850d34e5bdc1d19bc8a37dc907357d3a10
amir4code <69157292+amir4code@users.noreply.github.com>
2020-08-04 12:36:39 -0400

    Delete IWageCalculator.java

--- src/wizard/entity/calculators/IWageCalculator.java
+++
@@ -1,8 +0,0 @@
-package wizard.entity.calculators;
-
-public interface IWageCalculator {
-	
-	public int calculateWages(int startTime,int bedTime, int endTime, int rate1, int rate2, int rate3);
-
-}
-
